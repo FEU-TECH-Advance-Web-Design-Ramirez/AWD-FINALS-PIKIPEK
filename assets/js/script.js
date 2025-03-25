0a62d4e25f4cf72c370a845dd2895a0b7bf12a29
MELLERZPEEK <Chritianmgf29@gmail.com>
2025-03-25 23:36:24 +0800

    feat: login function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Check if user is already logged in
+// Check if user  is already logged in
 document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem("isLoggedIn") === "true") {
     document.getElementById("login-page").style.display = "none";
@@ -18,3 +18,20 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("event-details").value = savedService;
     }
     });
+    
+    // Login Function
+    function login(event) {
+        event.preventDefault();
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+       
+        if (username.trim() !== "" && password.trim() !== "") {
+        document.getElementById("login-page").style.display = "none";
+        document.getElementById("landing-page").style.display = "block";
+       
+        // Save login state
+        localStorage.setItem("isLoggedIn", "true");
+        } else {
+        alert("Please enter a username and password.");
+        }
+        }
